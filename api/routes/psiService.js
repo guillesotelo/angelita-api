15f5dc52a9c89160f106d629a1df8ee1cee653a1
Guillermo <gsotelo@volvocars.com>
2025-07-08 11:06:22 +0200

    psi-service

--- api/routes/psiService.js
+++ api/routes/psiService.js
@@ -3,109 +3,6 @@ const router = express.Router()
 const { PsiService } = require('../db/models')
 const { verifyToken } = require('../helpers')
 
- const SERVICES = [
-    {
-        title: 'PRIMERA CONSULTA DE 90 MINUTOS',
-        slug: 'primera-consulta-de-90-minutos',
-        subtitle: '',
-        description: `
-        €30`,
-
-        priceCOP: '90000',
-        priceEUR: '30',
-        paymentLink: '',
-        type: 'privada',
-        image: '',
-        active: true,
-        buy_button_id: "buy_btn_1RayekHIxmqjxScpOD0KUCqL",
-    },
-
-    {
-        title: 'CONSULTA INDIVIDUAL',
-        slug: 'consulta-individual',
-        subtitle: '',
-        description: `
-        €40 x 1 hora
-        \n
-        €60 x 2 horas`,
-
-        priceCOP: '130000',
-        priceCOP2: '100000',
-        priceEUR: '40',
-        priceEUR2: '60',
-        paymentLink: '',
-        type: 'privada',
-        image: '',
-        active: true,
-        buy_button_id: "buy_btn_1Raz5NHIxmqjxScpk3QOzo4C",
-    },
-
-    {
-        title: 'PAQUETE DE 4 SESIONES (Semanales y consecutivas)',
-        slug: 'paquete-de-4-sesiones',
-        subtitle: '',
-        description: `
-        €120 x 4 horas`,
-
-        priceCOP: '400000',
-        priceEUR: '120',
-        paymentLink: '',
-        type: 'privada',
-        image: '',
-        active: true,
-        buy_button_id: "buy_btn_1Raz6IHIxmqjxScpgmbVo1Wh",
-    },
-
-
-    {
-        title: 'GRUPO DE APOYO PSICOTERAPÉUTICO (4 sesiones, semanales y consecutivas. Jueves 18:00 horas de España)',
-        slug: 'grupo-de-apoyo-psicoterapeutico',
-        subtitle: '',
-        description: `
-        €40`,
-
-        priceCOP: '200000',
-        priceEUR: '40',
-        paymentLink: '',
-        type: 'grupal',
-        image: '',
-        active: true,
-        buy_button_id: "buy_btn_1Raz7CHIxmqjxScpYRMEcOpH",
-    },
-
-    {
-        title: 'GRUPO DE APOYO ESPIRITUAL (Sesiones semanales, sin continuidad fija)',
-        slug: 'grupo-de-apoyo-espiritual',
-        subtitle: '',
-        description: `
-        $ Aporte Voluntario`,
-
-        priceCOP: '0',
-        priceEUR: '0',
-        paymentLink: '',
-        type: 'grupal',
-        image: '',
-        active: true,
-        buy_button_id: "buy_btn_1RazAYHIxmqjxScpdVr9Ynou",
-    },
-
-    {
-        title: 'CHARLAS & ENCUENTROS FORMATIVOS (Online o presenciales. 4 horas)',
-        slug: 'charlas-y-encuentros-formativos',
-        subtitle: '',
-        description: `
-        €200
-        \n
-        (Valor de Transporte y Alojamiento en caso de ser presencial no está incluido)`,
-        priceCOP: '1000000',
-        priceEUR: '200',
-        paymentLink: '',
-        type: 'grupal',
-        image: '',
-        active: true,
-        buy_button_id: "buy_btn_1RazBKHIxmqjxScpmmuF6Fp3",
-    },
-]
 //Get all services
 router.get('/getAll', async (req, res, next) => {
     try {
@@ -132,18 +29,6 @@ router.get('/getById', async (req, res, next) => {
         if (!service) return res.status(404).send('Post not found.')
 
         res.status(200).json(service)
-    } catch (err) {
-        console.error('Something went wrong!', err)
-        res.send(500).send('Server Error')
-    }
-})
-
-//Create bulk
-router.get('/createBulk', async (req, res, next) => {
-    try {
-        const arr = await Promise.all(SERVICES.map(async s => await PsiService.create(s)))
-
-        res.status(200).json(arr)
     } catch (err) {
         console.error('Something went wrong!', err)
         res.send(500).send('Server Error')
